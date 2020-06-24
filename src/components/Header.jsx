6b7baf515177f60dc421744301d3455cfa37fee5
Am1k0 <suldina.o@gmail.com>
2020-06-25 00:43:31 +0300

    init commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+        <img src='https://w7.pngwing.com/pngs/809/200/png-transparent-blue-phoenix-logo-phoenix-leaf-logo-electric-blue.png'/>
+      </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className='nav'>
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>About</a>
+        </div>
+        <div>
+          <a>History</a>
+        </div>
+        <div>
+          <a>Contact us</a>
+        </div>
+      </nav>
+    )
+}
+
+export default Navbar;
